fede5544dfe416785995f84580c064e7f475c328
Dhivakaran-Kalanesan <123280511+Dhivakaran-Kalanesan@users.noreply.github.com>
2023-04-13 15:57:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+let bars = document.getElementById('bars') ;
+
+let xmark = document.getElementById('xmark') ;
+
+let navList = document.getElementById('navList') ;
+
+
+bars.addEventListener('click',()=>{
+    navList.style.left = "0";
+    navList.style.transition = ".3s" ;
+    navList.style.transitionTimingFunction = "ease-in-out" ;
+    navList.style.boxShadow = "0px 0px 40px rgb(202, 201, 201)" ;
+})
+
+
+xmark.addEventListener('click',()=>{
+    navList.style.left = "-100%";
+    navList.style.transition = ".3s" ;
+    navList.style.transitionTimingFunction = "ease-in-out" ;
+    navList.style.boxShadow = "none" ;
+})
